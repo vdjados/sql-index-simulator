40bdf335859027aa13cb63e9505e5c415b84a999
Vladislav <vladislav-bodnya@mail.ru>
2026-04-15 15:34:40 +0300

    Add tsx spa

--- internal/api/server.go
+++ internal/api/server.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"github.com/sirupsen/logrus"
@@ -22,6 +23,12 @@ func StartServer() {
 	_ = godotenv.Load() // загружает .env (DB_PASS, DB_HOST и т.д.)
 
 	router := gin.Default()
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000", "http://127.0.0.1:3000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization"},
+		AllowCredentials: true,
+	}))
 
 	postgresString := dsn.FromEnv()
 	logrus.Info("DSN: ", postgresString)
